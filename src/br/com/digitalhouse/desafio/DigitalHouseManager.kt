7360595c231d26825a3bc0a8878a78924830df93
Camila Pelegrina <camila.pelegrina@usp.br>
2020-09-28 10:20:17 -0300

    Classes Matricula e DigitalHouseManager

---
+++ src/br/com/digitalhouse/desafio/DigitalHouseManager.kt
@@ -0,0 +1,9 @@
+package br.com.digitalhouse.desafio
+
+class DigitalHouseManager() {
+    var listaAlunos = mutableListOf<Aluno>()
+    var listaProfessores = mutableListOf<Professor>()
+    var listaCursos = mutableListOf<Curso>()
+    var listaMatriculas = mutableListOf<Matricula>()
+
+}
---
+++ src/br/com/digitalhouse/desafio/Matricula.kt
@@ -0,0 +1,9 @@
+package br.com.digitalhouse.desafio
+
+import java.util.*
+
+class Matricula(var aluno: Aluno, var curso: Curso) {
+
+    var data = Date()
+
+}
